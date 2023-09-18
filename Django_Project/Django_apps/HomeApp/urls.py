dce93e730a9edf0f6a1360eca3b6d3655d7d1d99
FOA-AndyG <andy.g@enitiallab.com>
2023-09-18 12:51:54 -0700

    1.1

--- Django_Project/Django_apps/HomeApp/urls.py
+++ Django_Project/Django_apps/HomeApp/urls.py
@@ -15,14 +15,14 @@ urlpatterns = [
 
     path('logout/', views.logout, name='logout'),
 
-    path('home/', views.home, name='home'),
-    re_path(r'^home/', RedirectView.as_view(pattern_name='home', permanent=False)),
+    # path('home/', views.home, name='home'),
+    # re_path(r'^home/', RedirectView.as_view(pattern_name='home', permanent=False)),
 
-    path('new_home_page/', views.new_home_page, name='new_home_page'),
+    # path('new_home_page/', views.new_home_page, name='new_home_page'),
 
-    # dashboard
-    path('dashboard_total_upcoming_container_ajax/', views.dashboard_total_upcoming_container_ajax,
-         name='dashboard_total_upcoming_container_ajax'),
+    # # dashboard
+    # path('dashboard_total_upcoming_container_ajax/', views.dashboard_total_upcoming_container_ajax,
+    #      name='dashboard_total_upcoming_container_ajax'),
 
 
 ]
