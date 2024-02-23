11ed3d5d2bdafa4da26541a28ed87849a2c0a585
vlee-foa <115103879+vlee-foa@users.noreply.github.com>
2024-02-23 10:54:08 -0800

    po data size value

--- Django_Project/Django_apps/OSTKApiApp/functions/ostk_po.py
+++ Django_Project/Django_apps/OSTKApiApp/functions/ostk_po.py
@@ -30,4 +30,4 @@ def get_po_view(conn, receiving_code):
     view_q = f"""SELECT master_ref_no, product_barcode, received, total FROM po_sub where receiving_code = '{receiving_code}'"""
     view_df = pd.read_sql_query(view_q, conn)
     view_df.replace([pd.NaT], [None])
-    return list(view_df.itertuples(index=False))
+    return list(view_df.itertuples(index=False)), len(view_df)
